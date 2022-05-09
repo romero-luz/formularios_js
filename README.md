1254f58f504b46aec5229d727b278ba1e3207f17
Luz Dary Romero <103968881+romero-luz@users.noreply.github.com>
2022-05-09 17:59:57 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## 1. Hallar el mayor de tres numeros
 
 ## 2. Capturar un numero e imprimir el dia de la semana a que corresponde
+
+## 3. Imprimir todos los numeros entre 1 y n
+
+## 4. Capturar un numero e imprimir todos los numeros imares que existen entre 1 
+
+## 5. Capturar un valor entero
---
+++ codigo4_js.js
@@ -0,0 +1,17 @@
+let impar = "";
+let i = 1;
+
+function ciclowhile(objeto)
+{
+    numero = parseInt(document.formulario.caja.value);
+    while(i<=numero)
+    {
+        if(i%2!==0)
+        {
+            impar = impar + i + ",";
+        }
+        i = i+1;
+    }
+    document.formulario.respuesta.value = impar;
+
+}
